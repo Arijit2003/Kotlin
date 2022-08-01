a4f3aa642892c0209707086d24ea5c5bc1213c4f
Arijit Modak <arijitmodak2003@gmail.com>
2022-08-01 12:53:25 +0530

    Break and Continue

---
+++ functional programming/break-continue.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var I:Int=0
+    /*while(I<10){
+        println(I)
+        I++
+        if(I==5){
+            break
+        }
+    }*/
+    I=-1
+    while(I<10){
+        I++
+        if(I==5){
+            
+            continue
+        }
+        println(I)
+        
+    }
+}
---
+++ functional programming/while-loop.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var I:Int=0
+    while(I<10){
+        I++
+        println(I)
+        
+    }
+    var P=0
+    do{
+        println(P)
+        P++
+    }
+    while(P<10)
+}
